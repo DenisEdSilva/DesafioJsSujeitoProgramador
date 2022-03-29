d8df7b8092f8d5dc0d04cc2caf9f6769e3d0839d
Denis Eduardo <31224937+DenisEdSilva@users.noreply.github.com>
2022-03-29 14:09:05 -0400

    Add files via upload

--- DesafiosJs.js
+++ DesafiosJs.js
@@ -17,5 +17,41 @@ for (p in produtos) {
     console.log(`Não existe ${produtos[p]} na lista`)
   }
 }
-produtos.splice(1,1)
-console.log(produtos)
+produtos.splice(1,1);
+console.log(produtos);
+
+let num = [1,3,5,7,0,9];
+
+console.log(num.sort());
+num.shift();
+console.log(num);
+console.log(num.reverse());
+
+let hoje = '29/03/2022';
+
+const [novoDia, novoMes, novoAno] = hoje.split('/')
+
+console.log(`Estamos no dia: ${novoDia}`)
+console.log(`No mês: ${novoMes}`)
+console.log(`Do ano: ${novoAno}`)
+
+
+// Bonus pois interpretei errado a parte da data, achei q era pra fazer isso :p
+let data = new Date();
+console.log(data)
+
+const dia = data.getDate();
+console.log(`hoje é dia ${dia}`)
+
+const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+const mes = data.getMonth();
+
+for (m in meses) {
+  if (m == mes)
+  var nomeMes = meses[m]
+}
+
+console.log(`Nós estamos no mês de ${nomeMes}`)
+
+const ano = data.getFullYear();
+console.log(`E estamos no ano de ${ano}`)
